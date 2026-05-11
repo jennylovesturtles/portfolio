c86ac5f0f77b641e969e267826515b2c43048a2d
unknown <jennyzfire@gmail.com>
2026-05-11 14:30:59 -0500

    fixing hero

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -89,7 +89,8 @@ const Me = () => {
           <p style={description}>
             I identify engineering talent through more than resumes and keywords.
             My background in production systems gives me the ability to evaluate candidates
-            the way engineers evaluate each other.
+            the way engineers evaluate each other. I identify strong candidates through
+            signal-based sourcing.
           </p>
 
           <p style={description}>
